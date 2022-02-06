1c6a1b2cdb7edb104a767d16206cc922a5bf8cca
kattana <kattanaluz@gmail.com>
2022-02-06 11:36:18 +0000

    Styles

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,20 +1,32 @@
 import Image from "../../components/image";
-import H1 from "../../components/h1";
+import H1 from "../h1";
+import H2 from "../h2";
 import landImage from "./land_image.png";
 import Button from "../../components/button";
 import css from "./home.module.css";
 
 export function Home() {
-  const landText = `Thirsty? We have hundreds and hundreds of delicious cocktail recipes from expert bartenders around the world. Make yourself at home and find out a new drink recipe.`;
+  const landText = `Find out hundreds of delicious cocktail recipes from expert bartenders around the world.`;
   const buttonText = `search a recipe`;
 
   return (
-    <div className={css.container}>
-      <Image src={landImage} alt={"image"} className={css.landPageImage} />
-      <H1
+    <div>
+      <div className={css.h1AndImageContainer}>
+        <H1
+          h1ClassName={css.landPageH1}
+          divH1ClassName={css.divH1}
+          text="Fancy a drink?"
+        />
+        <Image
+          src={landImage}
+          alt={"Fancy a drink?"}
+          className={css.landPageImage}
+        />
+      </div>
+      <H2
         text={landText}
-        h1ClassName={css.landPageH1}
-        divH1ClassName={css.divH1}
+        h2ClassName={css.landPageH2}
+        divH2ClassName={css.divH2}
       />
       <Button text={buttonText} className={css.homeButton} />
     </div>
